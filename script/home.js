bc74d4df413c5aad7635e501cfa3b7969ccbbde0
Andrey <andrey.redmi.pro16@gmail.com>
2025-10-23 16:34:51 +0300

    NEW

--- script/home.js
+++ script/home.js
@@ -3,6 +3,18 @@ function displayParams() {
    
     const savedParams = sessionStorage.getItem('counter') || 0;
     counterElement.textContent = savedParams;
+
+    const a = document.getElementById('4.1');
+    const b = document.getElementById('4.2');
+    const c = document.getElementById('4.3');
+    const d = document.getElementById('4.4');
+    const e = document.getElementById('4.5');
+
+    a.style.visibility = sessionStorage.getItem('4.1') || 'visible';
+    b.style.visibility = sessionStorage.getItem('4.2') || 'visible';
+    c.style.visibility = sessionStorage.getItem('4.3') || 'visible';
+    d.style.visibility = sessionStorage.getItem('4.4') || 'visible';
+    e.style.visibility = sessionStorage.getItem('4.5') || 'visible';
 }
 
 displayParams();
